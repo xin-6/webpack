9337bf61f2e03333fd6fc4237349a449e650088d
xin-6 <xin.6@outlook.com>
2017-05-02 10:09:00 +0800

    Update main.js

--- template/src/main.js
+++ template/src/main.js
@@ -4,11 +4,24 @@
 {{/if_eq}}
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+import axios from 'axios'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 {{#router}}
-import router from './router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import routes from './config/routes'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{/router}}
 
+Vue.use(VueRouter);
+Vue.use(ElementUI);
+Vue.use(Vuex);
+  
 Vue.config.productionTip = false{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
+const router = new VueRouter({
+    routes
+})
 
 /* eslint-disable no-new */
 new Vue({
@@ -16,11 +29,13 @@ new Vue({
   {{#router}}
   router,
   {{/router}}
-  {{#if_eq build "runtime"}}
-  render: h => h(App){{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  {{/if_eq}}
-  {{#if_eq build "standalone"}}
-  template: '<App/>',
-  components: { App }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+  render: (h) => h(App)
   {{/if_eq}}
 }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
+// 引入自己封装的跨域请求代码，不需要请注释掉
+// function Jsonp(url,h){this.data='';var s=document.createElement("script");s.src=url;document.head.appendChild(s);callback=function(d,f){this.data=d;h(this.data)}}
+// 用法		注意，回掉函数名一定要改成callback
+//var jsonp = new Jsonp("https://sug.so.360.cn/suggest?callback=callback&word=a", function(data){
+//    console.log(data.s);
+//});

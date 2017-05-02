0f4cd8c4484e27c4b809d82caa9fedf3cf1e4ec8
xin-6 <xin.6@outlook.com>
2017-05-02 10:47:25 +0800

    Update main.js

--- template/src/main.js
+++ template/src/main.js
@@ -1,23 +1,17 @@
-{{#if_eq build "standalone"}}
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-{{/if_eq}}
-import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import Vue from 'vue'
+import App from './App'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 import axios from 'axios'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
-{{#router}}
-import routes from './config/routes'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-{{/router}}
+import routes from './config/routes'
 
 Vue.use(VueRouter);
 Vue.use(ElementUI);
 Vue.use(Vuex);
   
-Vue.config.productionTip = false{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+Vue.config.productionTip = false
 
 const router = new VueRouter({
     routes
@@ -26,12 +20,9 @@ const router = new VueRouter({
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  {{#router}}
   router,
-  {{/router}}
   render: (h) => h(App)
-  {{/if_eq}}
-}){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+}
 
 // 引入自己封装的跨域请求代码，不需要请注释掉
 // function Jsonp(url,h){this.data='';var s=document.createElement("script");s.src=url;document.head.appendChild(s);callback=function(d,f){this.data=d;h(this.data)}}
